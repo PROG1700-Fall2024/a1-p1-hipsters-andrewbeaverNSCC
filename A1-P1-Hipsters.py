8f8e726caafb1616348b432d867217fab601a745
Your Name <w0402993@nscc.ca>
2024-10-05 16:37:52 -0300

    Cleaned some lined of code

--- A1-P1-Hipsters.py
+++ A1-P1-Hipsters.py
@@ -1,13 +1,12 @@
 #Program 1 – Hipster's Local Vinyl Records
 #Hipster’s Local Vinyl Records sell and hand-deliver vinyl records to their customers.
-
-#Student Name: Andrew Beaver
-#Program Title: Hipster's Local Vinyl Records
-#Description:  Assignment 1 Part 1
+"""
+Student Name: Andrew Beaver
+Program Title: Hipster's Local Vinyl Records
+Description:  Assignment 1 Part 1
+"""
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
     #Display opening message to the user
     print("Hipter's Local Vinyl Records - Customer Order Details")
 
@@ -24,16 +23,17 @@ def main():
     deliveryCost = totalKM * 15
     purchaseCost = recordCost * 1.14
     totalCost = purchaseCost + deliveryCost
+    
     #Display summary with name
     print("\nPurchase summary for",customerName)
 
     #display Delivery cost
     print("Delivery Cost: ${0:,.2f}".format(deliveryCost))
+    
     #Display purchase cost + cost
     print("Purchase Cost: ${0:,.2f}".format(purchaseCost))
+    
     #Display total
     print("Total Cost   : ${0:,.2f}".format(totalCost))
-
-    # YOUR CODE ENDS HERE
 
 main()
